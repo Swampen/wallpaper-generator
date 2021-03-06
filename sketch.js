0aa135617f8091d8093f86401daf9006727120a3
Ole-Martin Heggen <ole-ma@outlook.com>
2021-03-06 18:15:26 +0100

    Made the first mountain range

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
+/// @ts-check
+/// <reference path="./libraries/ts-types/global.d.ts" />
+
+
+let ridgeFactor = 10;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+	background(51);
+	//noFill();
+	beginShape();
+	stroke(255);
+	vertex(0, windowHeight)
+	let noiceOff = 0
+	for (let x = 0; x < windowWidth; x += ridgeFactor) {
+		let y = noise(noiceOff)*height;
+		vertex(x, y);
 
+		noiceOff += 0.01;
+	}
+	vertex(windowWidth, windowHeight);
+	endShape();
+	noLoop();
 }
